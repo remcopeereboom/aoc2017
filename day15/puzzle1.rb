99d652b66ac472718de70a41e45a87f5aa7ed75d
R. Peereboom <r.peereboom@yandex.com>
2017-12-15 11:11:56 +0100

    Finished day 15

---
+++ day15/puzzle1.rb
@@ -0,0 +1,41 @@
+################################################################################
+# Takes two lines of input, each with an integer describing the initial state  #
+# of a generator. Runs a duel between both generators and returns the number   #
+# of ties between them.                                                        #
+#                                                                              #
+# To run: ruby puzzle1.rb input                                                #
+################################################################################
+
+module Duel
+  def self.generator(state, factor, divisor)
+    Enumerator.new do |y|
+      loop do
+        state = (state * factor) % divisor
+        y << state
+      end
+    end
+  end
+
+  def self.judge(a, b, n = 40_000_000)
+    low_order_bits_16 = 65_535 # 1111_1111_1111_1111_1111
+    n.times.count do |i|
+      a.next & low_order_bits_16 == b.next & low_order_bits_16
+    end
+  end
+end
+
+def self.initial_state_from(istream)
+  ARGF.gets.chomp.match(/(\d+)/)[1].to_i
+end
+
+a_state = initial_state_from(ARGF)
+b_state = initial_state_from(ARGF)
+a_factor = 16807
+b_factor = 48271
+a_divisor = 2147483647
+b_divisor = 2147483647
+
+generator_a = Duel.generator(a_state, a_factor, a_divisor)
+generator_b = Duel.generator(b_state, b_factor, b_divisor)
+
+puts Duel.judge(generator_a, generator_b)
